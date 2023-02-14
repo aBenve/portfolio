41929275756b0adde883f3063ceaf5761c58da23
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2023-02-14 18:39:16 -0300

    Add projects with images and add portfolio pdf

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,6 @@
-import img from '../assets/probablyMockup.png'
+import probablyMockup from '../assets/probablyMockup.png'
+import wavamMockup from '../assets/wavamMockup.png'
+import schedulerMockup from '../assets/schedulerMockup.png'
 
 
 const data = [
@@ -10,7 +12,27 @@ const data = [
         desc: "Probability and statistics web calculator.",
         descColor: "#202020",
         bgColor: "#A594FE",
-        image: img
+        image: probablyMockup
+    },
+    {
+        gender: "Sitio Web",
+        genderColor: "#fafafa",
+        title: "wavam",
+        titleColor: "#fafafa",
+        desc: "Podcast web application. People can upload, search and listen to podcasts.",
+        descColor: "#202020",
+        bgColor: "#F2DBA4",
+        image: wavamMockup
+    },
+    {
+        gender: "Sitio Web",
+        genderColor: "#fafafa",
+        title: "scheduler",
+        titleColor: "#fafafa",
+        desc: "Website where ITBA students can schedule their term's classes.",
+        descColor: "#202020",
+        bgColor: "#4F6FE8",
+        image: schedulerMockup
     }
 ]
 
